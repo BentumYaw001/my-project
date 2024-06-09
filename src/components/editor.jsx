526890f6840a4b434ff2b201dc6f2a1dfb17d7e9
BentumYaw001 <richmondbentum2020@gmail.com>
2024-06-09 01:02:14 -0700

    hide preview toggle

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -1,20 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import "./customScrollbar.css"
+import "./customScrollbar.css";
 
 // eslint-disable-next-line react/prop-types
 function Editor({ markdown, setMarkdown }) {
   return (
-    <div className=' border-r-2 border-gray-600'>
+    <div className="border-r-2 border-gray-600">
       <div className="w-full bg-customblack1 p-3 text-gray-400 uppercase tracking-wider">
         Markdown
       </div>
       <textarea
-        className=".custom-scrollbar w-full h-full bg-mainblack text-white outline-none p-6"
-        onChange={(e) => setMarkdown(e.target.value)} 
-      >
-        {markdown}
-      </textarea>
+        className="custom-scrollbar w-full h-full bg-mainblack text-white outline-none p-6"
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
     </div>
   );
 }
